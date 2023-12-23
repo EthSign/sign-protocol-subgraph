b7f58056cc2e3af04d99cf67ddfeed495aed2cb7
boyuanx <boyuanxu@usc.edu>
2023-12-23 17:49:21 +0800

    Draft

--- src/sap.ts
+++ src/sap.ts
@@ -1,115 +1,82 @@
+import { Address, Bytes } from "@graphprotocol/graph-ts";
 import {
   AttestationMade as AttestationMadeEvent,
   AttestationRevoked as AttestationRevokedEvent,
-  Initialized as InitializedEvent,
   OffchainAttestationMade as OffchainAttestationMadeEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
+  OffchainAttestationRevoked as OffchainAttestationRevokedEvent,
   SchemaRegistered as SchemaRegisteredEvent,
-  Upgraded as UpgradedEvent
-} from "../generated/SAP/SAP"
-import {
-  AttestationMade,
-  AttestationRevoked,
-  Initialized,
-  OffchainAttestationMade,
-  OwnershipTransferred,
-  SchemaRegistered,
-  Upgraded
-} from "../generated/schema"
+  SAP,
+} from "../generated/SAP/SAP";
+import { Schema, Attestation, OffchainAttestation } from "../generated/schema";
+
+function addressArrayToBytesArray(addressArray: Address[]): Bytes[] {
+  let bytesArray: Bytes[] = [];
+  for (let i = 0; i < addressArray.length; i++) {
+    bytesArray.push(addressArray[i]);
+  }
+  return bytesArray;
+}
+
+function dataLocationNumberToEnumString(dataLocation: number): string {
+  if (dataLocation == 0) {
+    return "ONCHAIN";
+  } else if (dataLocation == 1) {
+    return "ARWEAVE";
+  } else {
+    return "UNSUPPORTED";
+  }
+}
 
 export function handleAttestationMade(event: AttestationMadeEvent): void {
-  let entity = new AttestationMade(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.attestationId = event.params.attestationId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = new Attestation(event.params.attestationId);
+  const attestation = SAP.bind(event.address).attestationRegistry(
+    event.params.attestationId
+  );
+  entity.schema = attestation.schemaId;
+  entity.linkedAttestation = attestation.linkedAttestationId;
+  entity.attester = event.transaction.from;
+  entity.attestTimestamp = event.block.timestamp;
+  entity.validUntil = attestation.validUntil;
+  entity.recipients = addressArrayToBytesArray(attestation.recipients);
+  entity.save();
 }
 
 export function handleAttestationRevoked(event: AttestationRevokedEvent): void {
-  let entity = new AttestationRevoked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.attestationId = event.params.attestationId
-  entity.reason = event.params.reason
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleInitialized(event: InitializedEvent): void {
-  let entity = new Initialized(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.version = event.params.version
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = Attestation.load(event.params.attestationId)!;
+  entity.revoked = true;
+  entity.revokeReason = event.params.reason;
+  entity.revokeTimestamp = event.block.timestamp;
+  entity.save();
 }
 
 export function handleOffchainAttestationMade(
   event: OffchainAttestationMadeEvent
 ): void {
-  let entity = new OffchainAttestationMade(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.attestationId = event.params.attestationId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = new OffchainAttestation(event.params.attestationId);
+  entity.attestTimestamp = event.block.timestamp;
+  entity.save();
 }
 
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
+export function handleOffchainAttestationRevoked(
+  event: OffchainAttestationRevokedEvent
 ): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = OffchainAttestation.load(event.params.attestationId)!;
+  entity.revoked = true;
+  entity.revokeTimestamp = event.block.timestamp;
+  entity.revokeReason = event.params.reason;
+  entity.save();
 }
 
 export function handleSchemaRegistered(event: SchemaRegisteredEvent): void {
-  let entity = new SchemaRegistered(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.schemaId = event.params.schemaId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUpgraded(event: UpgradedEvent): void {
-  let entity = new Upgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let entity = new Schema(event.params.schemaId);
+  const schema = SAP.bind(event.address).schemaRegistry(event.params.schemaId);
+  entity.registrant = event.transaction.from;
+  entity.revocable = schema.revocable;
+  entity.dataLocation = dataLocationNumberToEnumString(schema.dataLocation);
+  entity.maxValidFor = schema.maxValidFor;
+  entity.resolver = schema.resolver;
+  entity.schema = schema.schema;
+  entity.registerTimestamp = event.block.timestamp;
+  entity.numberOfAttestations = 0;
+  entity.save();
 }
